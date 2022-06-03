c66d83ea5267dbc6d8529fe8f7f4349dfdcac207
asger1537 <34832830+asger1537@users.noreply.github.com>
2022-06-03 17:36:30 +0200

    New cleaning tests

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from cleantext.sklearn import CleanTransformer # likely required to ´pip instal
 from cleantext import clean
 import time
 #data = pd.read_csv("https://raw.githubusercontent.com/several27/FakeNewsCorpus/master/news_sample.csv")
-data = pd.read_csv(r"C:\Users\Computer\Documents\GitHub\Data-Science-Course-Work\CSVs\1mio-raw.csv", nrows = 1000)
+data = pd.read_csv(r"C:\Users\Computer\Documents\GitHub\Data-Science-Course-Work\CSVs\1mio-raw.csv", nrows = 1500)
 
 def drop_useless_data(data):
     #Dropping unneeded columns
@@ -34,19 +34,13 @@ def drop_useless_data(data):
     data.dropna(subset = ["type"], inplace = True)
     #Dropping entries with unknown type
     data.drop(data.loc[data["type"] == "unknown"].index, inplace=True)
-    not_enough_of_type = ["clickbait", "reliable", "unreliable", "bias", "hate", "junksci"]
-    for t in not_enough_of_type:
-        data.drop(data.loc[data["type"] == t].index, inplace=True)
 
 #drop_useless_data(data)
 print("data size after initial  drop:", data.shape[0])
-# hree_numb_date = r'(<number> <number> <number>)' #YYYY/MM/DD or DD/MM/YYYY or MM/DD/YYYY
-# literal_months_date= r'(jan|feb|mar|apr|may|jun|jul|aug|sep|nov|dec)\S* ((<number> ){1,2}|([0-9]{1,2}(st|nd|rd|th)))' #Eg. jun 2nd 2020, january 23. 2021
-# literal_months_reverse_date = r'((number {1,2})|[0-9]{1,2}(st|nd|rd|th)) *(jan|feb|mar|apr|may|jun|jul|aug|sep|nov|dec)\S*' #Eg. 10th february, 4th july
-# all_dates = (three_numb_date) +'|' + (literal_months_date) +'|'+ (literal_months_reverse_date)
-# multiple_chars = r'(.)\1{3,}'
-# special_symbols = r'([^<>a-z ])'#Matches special symbols such as © or ™
-# single_letter = r' [a-z] ' #matches single letters
+multiple_chars = r'(.)\1{3,}'
+special_symbols = r'([^<>a-z ])'#Matches special symbols such as © or ™
+single_letter = r' [a-z] ' #matches single letters
+temp = r'<0>|<url>|<cur>|<email>|<phone>'
 
 initial_cleaner = CleanTransformer(fix_unicode=True,               # fix various unicode errors
                                     to_ascii=True,                  # transliterate to closest ASCII representation
@@ -59,17 +53,33 @@ initial_cleaner = CleanTransformer(fix_unicode=True,               # fix various
                                     no_digits=True,                # replace all digits with a special token
                                     no_currency_symbols=True,      # replace all currency symbols with a special token
                                     no_punct=True,                 # remove punctuations
+                                    #replace_with_punct=" ",          # instead of removing punctuations you may replace them
+                                    # replace_with_url="",
+                                    # replace_with_email="",
+                                    # replace_with_phone_number="",
+                                    # replace_with_number="",
+                                    # replace_with_currency_symbol="",
+                                    # replace_with_digit="",   
                                     lang="en"                       # set to 'de' for German special handling
                                     )
 
+print(re.sub(temp, "", "in <number>. mr. wheelers"))
+
+
 def clean_column(data, col_name):
-    for i, entry in zip(data[col_name].index, data[col_name]):    
-        #We first convert to lower case and replace punctuation with space such that dates can
-        #more easily be processed (eg. 10.12.2020 -> 10 12 2020 -> <NUMBER> <NUMBER> <NUMBER> instead of <NUMBER><NUMBER><DIGIT> or something)
+    for i, entry in zip(data[col_name].index, data[col_name]):
+        if i % 1000 == 0:
+            print(f"cleaned {i} from {col_name}")
+        # adds approx. 0.35 pr. 1500
         cleaned = initial_cleaner.transform([entry])[0]
-        #cleaned = re.sub(special_symbols,'',cleaned)
-        #cleaned = re.sub(multiple_chars, '', cleaned)
-        #cleaned = re.sub(single_letter, '',cleaned)
+        cleaned = re.sub(special_symbols,'',cleaned)
+        cleaned = re.sub(multiple_chars, '', cleaned)
+        cleaned = re.sub(single_letter, '',cleaned)
+        
+        cleaned = re.sub(temp, '',cleaned)
+        
+        
+
         data.at[i, col_name] = cleaned
 
 def clean_data(data):
@@ -87,9 +97,9 @@ def clean_data(data):
 
     print(f"content = {content_clean_time}, title: {title_clean_time}") 
 
-
 clean_data(data)
 
+print(data["content"][0])
 
 # 0,00875 
 # 8.750
